850ffe2248a5946625745c57d9e52f37a4b3ae69
YatseaLi <yatsealee@gmail.com>
2023-04-28 01:05:55 +1000

    implement InboundCustomerMessage create event handler

--- customer-interaction-llm/src/srv/admin-service.js
+++ customer-interaction-llm/src/srv/admin-service.js
@@ -1,4 +1,6 @@
 const cds = require("@sap/cds/lib");
+const e = require("express");
+
 module.exports = class AdminService extends cds.ApplicationService {
   init() {
     const {
@@ -15,17 +17,38 @@ module.exports = class AdminService extends cds.ApplicationService {
       genseq
     );
 
-    // this.before(["NEW", "CREATE"], InboundCustomerMessage, async (req) => {
-    //   const { sequence } = await cds
-    //     .tx(req)
-    //     .run(
-    //       SELECT.one
-    //         .from(InboundCustomerMessage)
-    //         .columns("max(sequence) as sequence")
-    //         .where("interaction_ID=", req.data.interaction_ID)
-    //     );
-    //   req.data.sequence = sequence + 1;
-    // });
+    /**
+     * Handler of on creating an inbound customer message
+     * 1.invoke the llm-proxy to perform sentiment analysis,
+     * text summarisation and entity extraction
+     * 2.update the sentiment on the InboundCustomerMessage instance.
+     * 3.update the title and summary on parent object CustomerInteraction
+     */
+    this.on(["CREATE"], InboundCustomerMessage, async (req) => {
+      const inboundTextMsgs = await cds
+        .tx(req)
+        .run(
+          SELECT.from(InboundCustomerMessage)
+            .columns("inboundTextMsg")
+            .where("interaction_ID=", req.data.interaction_ID)
+        );
+      let inMsgs = inboundTextMsgs.map(msg => msg.inboundTextMsg);
+      inMsgs.push(req.data.inboundTextMsg);
+      const text = { text: inMsgs.join("\n") };
+      const LlmProxyService = await cds.connect.to("LlmProxyService");
+      const result = await LlmProxyService.processCustomerMessage(text);
+      const embedding = await LlmProxyService.embedding(text);
+      req.data.vector = embedding;
+      cds.tx (async ()=>{
+        await UPDATE(CustomerInteraction, req.data.interaction_ID).with({
+          title: result.data.title,
+          summary: result.data.summary
+        })
+        await INSERT.into(InboundCustomerMessage, req.data);
+      });
+
+      return req.data;
+    });
 
     return super.init();
   }
