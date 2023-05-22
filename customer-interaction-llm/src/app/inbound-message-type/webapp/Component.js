aa7dcd932d3afd3555907063b7941ac1a494b739
YatseaLi <yatsealee@gmail.com>
2023-05-22 16:01:25 +1000

    add inbound-message-type app

---
+++ customer-interaction-llm/src/app/inbound-message-type/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("inbound_message_type.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
